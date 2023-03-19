9df555bd9c0dd451a16b2d51aeddefa2154e2094
Kristian Underdal <kriu@stud.ntnu.no>
2023-03-20 00:04:40 +0100

    La til 52 kortbilder

--- src/main/java/BlackJackProject/SceneController.java
+++ src/main/java/BlackJackProject/SceneController.java
@@ -10,32 +10,28 @@ import javafx.stage.Stage;
 
 public class SceneController {
 
-    private Parent parent;
-    private Scene scene;
-    private Stage stage;
-
     protected void switchToNewGame(ActionEvent event) throws IOException {
-        parent = FXMLLoader.load(getClass().getResource("NewGame.fxml"));
-        scene = new Scene(parent);
+        Parent parent = FXMLLoader.load(getClass().getResource("NewGame.fxml"));
+        Scene scene = new Scene(parent);
         setNewScene(event, scene);
     }
 
     protected void switchToSavedGame(ActionEvent event) throws IOException {
-        parent = FXMLLoader.load(getClass().getResource("SavedGame.fxml")); // Kan komme endringer, skal hente tilstand fra fil
-        scene = new Scene(parent);
+        Parent parent = FXMLLoader.load(getClass().getResource("SavedGame.fxml")); // Kan komme endringer, skal hente tilstand fra fil
+        Scene scene = new Scene(parent);
         setNewScene(event, scene);
     }
 
     protected void switchToStartScreen(ActionEvent event) throws IOException {
-        parent = FXMLLoader.load(getClass().getResource("StartScreen.fxml"));
-        scene = new Scene(parent);
+        Parent parent = FXMLLoader.load(getClass().getResource("StartScreen.fxml"));
+        Scene scene = new Scene(parent);
         setNewScene(event, scene);
     }
 
     private void setNewScene(ActionEvent event, Scene newScene) {
         Node eventSource = (Node) event.getSource();
-        stage = (Stage) eventSource.getScene().getWindow();
-        stage.setScene(scene);
+        Stage stage = (Stage) eventSource.getScene().getWindow();
+        stage.setScene(newScene);
         stage.show();
     }
 
