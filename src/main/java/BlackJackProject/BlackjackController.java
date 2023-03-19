cdd0a386d8b26f21d91e246f357d8199245397ec
Kristian Underdal <kriu@stud.ntnu.no>
2023-03-19 22:15:40 +0100

    La til funksjonalitet i SceneController

--- src/main/java/BlackJackProject/BlackjackController.java
+++ src/main/java/BlackJackProject/BlackjackController.java
@@ -1,5 +1,18 @@
 package blackjackproject;
 
-public class BlackjackController {
-    
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+
+public class BlackjackController extends SceneController {
+
+    @Override
+    public void switchToNewGame(ActionEvent event) throws IOException {
+        super.switchToNewGame(event);
+    }
+
+    @Override
+    public void switchToStartScreen(ActionEvent event) throws IOException {
+        super.switchToStartScreen(event);
+    }
 }
--- src/main/java/BlackJackProject/SceneController.java
+++ src/main/java/BlackJackProject/SceneController.java
@@ -1,5 +1,42 @@
 package blackjackproject;
 
+import java.io.IOException;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 public class SceneController {
-    
+
+    private Parent parent;
+    private Scene scene;
+    private Stage stage;
+
+    protected void switchToNewGame(ActionEvent event) throws IOException {
+        parent = FXMLLoader.load(getClass().getResource("NewGame.fxml"));
+        scene = new Scene(parent);
+        setNewScene(event, scene);
+    }
+
+    protected void switchToSavedGame(ActionEvent event) throws IOException {
+        parent = FXMLLoader.load(getClass().getResource("SavedGame.fxml")); // Kan komme endringer, skal hente tilstand fra fil
+        scene = new Scene(parent);
+        setNewScene(event, scene);
+    }
+
+    protected void switchToStartScreen(ActionEvent event) throws IOException {
+        parent = FXMLLoader.load(getClass().getResource("StartScreen.fxml"));
+        scene = new Scene(parent);
+        setNewScene(event, scene);
+    }
+
+    private void setNewScene(ActionEvent event, Scene newScene) {
+        Node eventSource = (Node) event.getSource();
+        stage = (Stage) eventSource.getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
+
 }
