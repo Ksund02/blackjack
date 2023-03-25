c9a59da1d0fa472220771868ae5ab9f52a90b375
Kristian Underdal <kriu@stud.ntnu.no>
2023-03-25 18:05:02 +0100

    Endringer i NewGame.fxml

--- src/main/java/BlackJackProject/BlackjackController.java
+++ src/main/java/BlackJackProject/BlackjackController.java
@@ -3,8 +3,20 @@ package blackjackproject;
 import java.io.IOException;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 
 public class BlackjackController extends SceneController {
+
+    @FXML Button increaseButton;
+    @FXML Button decreaseButton;
+    @FXML Label betAmount;
+
+    @FXML
+    public void increaseBet() {
+        
+    }
 
     @Override
     public void switchToNewGame(ActionEvent event) throws IOException {
