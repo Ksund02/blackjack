55f2a7aad4962abe2479d5519b374e81fed7b667
Kristian Underdal <kriu@stud.ntnu.no>
2023-03-25 13:32:18 +0100

    La til musikk-funksjonalitet

--- src/main/java/BlackJackProject/BlackjackApp.java
+++ src/main/java/BlackJackProject/BlackjackApp.java
@@ -1,12 +1,16 @@
 package blackjackproject;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
+import java.net.URL;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
 
 public class BlackjackApp extends Application {
@@ -17,10 +21,19 @@ public class BlackjackApp extends Application {
         Scene startScreen = new Scene(parent);
         Image logo = new Image(getClass().getResourceAsStream("Blackjack.png"));
         
+        playMusic("JazzOOP.mp3");
         stage.getIcons().add(logo);
         stage.setTitle("Blackjack");
         stage.setScene(startScreen);
         stage.show();
+    }
+
+    MediaPlayer mediaPlayer;
+    public void playMusic(String fileName) throws FileNotFoundException {
+        URL filePath = getClass().getResource(fileName);
+        mediaPlayer = new MediaPlayer(new Media(filePath.toString()));
+        mediaPlayer.setCycleCount(MediaPlayer.INDEFINITE);
+        mediaPlayer.play();
     }
 
     public static void main(String[] args) {
